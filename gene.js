125cb2f563ee1192dbcd6c1d04c904eed79979bf
butaosuinu <buta.osuinu@gmail.com>
2015-07-29 20:34:52 +0900

    add DL file name

--- gene.js
+++ gene.js
@@ -24,15 +24,13 @@ function DJRNGenrate() {
 		ctx.textAlign = "center";
 		ctx.fillText(getFormData, 275, 95, 500);
 		// png生成
-		// canvas.name = "DJRN_PNG";
 		var img_png = canvas.toDataURL("image/png");
 		document.getElementById("pngDL").href = img_png;
-		document.getElementById("pngDL").download = img_png;
-
-		// canvas.name = "DJRN_JPG";
+		document.getElementById("pngDL").download = "DJRN_PNG";
+		// JPG生成
 		var img_jpg = canvas.toDataURL("image/jpeg", "0.5");
 		document.getElementById("jpgDL").href = img_jpg; 
-		document.getElementById("jpgDL").download = img_jpg;
+		document.getElementById("jpgDL").download = "DJRN_JPG";
 		
 	}
 };
