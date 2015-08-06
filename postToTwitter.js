3701e67fcafddaa47f748152e4b14de2890a1a8a
buta <buta.osuinu@gmail.com>
2015-08-07 00:11:36 +0900

    wip server

--- postToTwitter.js
+++ postToTwitter.js
@@ -1,33 +1,37 @@
 var express = require('express'),
-	hogen = require('hogen-express'),
+	hogen = require('hogan-express'),
 	twitter = require('twitter'),
-	oauth = require('oauth').OAuth,
+	OAuth = require('node-oauth'),
 	http = require('http'),
-	path = require('path'),
-	gene = require('./gene.js');
+	path = require('path');
 
-var = app = express();
+var app = express();
 
-app.configure(function () {
-	app.set('port', process.env.PORT || 7000);
-	app.set('views', path.join(__dirname,"views"));
-	app.set("view engine", "html");
-	app.engine("html", hogen);
-	app.use(express.favicon());
+app.set('port', process.env.PORT || 7000);
+app.set('views', path.join(__dirname,"/"));
+app.set("view engine", "html");
+app.engine("html", hogen);
+
+app.get("/", function(req, res){
+	// body...
+	res.render("index",{});
 });
 
-app.get('/', index);
+http.createServer(app).listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
+});
 
-var oa = new OAuth(
-	"https://api.twitter.com/oauth/request_token",
-	"https://api.twitter.com/oauth/access_token",
-	"Consumer key",
-	"Consumer secret",
-	"1.0",
-	"http://127.0.0.1:7000/auth/twiiter/callback",
-	"HMAC-SHA1"
-);
 
-app.get('/auth/twitter', function(req, res) {
-	// body...
-})
+// var oa = new OAuth(
+// 	"https://api.twitter.com/oauth/request_token",
+// 	"https://api.twitter.com/oauth/access_token",
+// 	"Consumer key",
+// 	"Consumer secret",
+// 	"1.0",
+// 	"http://127.0.0.1:7000/auth/twiiter/callback",
+// 	"HMAC-SHA1"
+// );
+
+// app.get('/auth/twitter', function(req, res) {
+// 	// body...
+// });
