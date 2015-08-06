d4ee1ed84548fa49d6b6d131971f5b7fbfbedc0a
buta <buta.osuinu@gmail.com>
2015-08-06 22:48:07 +0900

    twiiter wip

--- postToTwitter.js
+++ postToTwitter.js
@@ -17,3 +17,17 @@ app.configure(function () {
 });
 
 app.get('/', index);
+
+var oa = new OAuth(
+	"https://api.twitter.com/oauth/request_token",
+	"https://api.twitter.com/oauth/access_token",
+	"Consumer key",
+	"Consumer secret",
+	"1.0",
+	"http://127.0.0.1:7000/auth/twiiter/callback",
+	"HMAC-SHA1"
+);
+
+app.get('/auth/twitter', function(req, res) {
+	// body...
+})
